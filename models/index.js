f70def80f4c10360329e7267a45afd2b72b28662
Kynan21 <abidzardhava@gmail.com>
2025-12-19 07:13:18 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,23 +1,19 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-// koneksi database 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'laundry.sqlite'
 });
 
-// model Customer
 const Customer = sequelize.define('Customer', {
   name: DataTypes.STRING
 });
 
-// model Order (Pesanan)
 const OrderDB = sequelize.define('Order', {
   jenis: DataTypes.STRING,
   jumlah: DataTypes.INTEGER
 });
 
-// relasi 1 customer punya banyak order (one to many)
 Customer.hasMany(OrderDB);
 OrderDB.belongsTo(Customer);
 
@@ -26,3 +22,4 @@ module.exports = {
   Customer,
   OrderDB
 };
+
